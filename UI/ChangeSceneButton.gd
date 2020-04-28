43a9ed3a4ce4277eeb85f7657dae662c6b8f655d
Andreas <roflcoffel@gmail.com>
2020-04-28 05:31:45 +0200

    Menu and UI

---
+++ UI/ChangeSceneButton.gd
@@ -0,0 +1,12 @@
+tool
+extends Button
+
+export(String, FILE) var next_scene : String = ""
+
+func _on_button_up():
+	get_tree().paused = false
+	get_tree().change_scene(next_scene)
+
+
+func _get_configuration_warning() -> String:
+	return "next_scene is not set!" if next_scene == "" else ""
